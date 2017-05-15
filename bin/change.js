69d8fcefd691f235685285520903d83d274bd4bd
liuxiaodong <liuxiaodong1989@gmail.com>
2017-05-15 10:24:09 +0800

    数据修改

--- bin/change.js
+++ bin/change.js
@@ -1,24 +1,11 @@
 const Page = require('../model').Page;
 
-Page.update({
-	retryTimes: {
-		$exists: false
+Page.update({}, {$set: {
+	expectFetchTime: {
+		hour: 0,
+		minute: 0
 	}
-}, {retryTimes: 0}, {multi: true}).then(function(ret){
-	console.log('update retryTimes success ', ret);
-	return Page.update({
-		canFetchTime: {
-			$exists: false
-		}
-	}, {canFetchTime: Date.now()}, {multi: true});
-}).then(function(ret) {
-	console.log('update canFetchTime complete ', ret);
-	return Page.update({
-		status: {
-			$exists: false
-		}
-	}, {status: 'normal'}, {multi: true});	
-}).then(function(ret) {
+}}, {multi: true}).then(function(ret){
 	console.log('update status complete ', ret);
 	process.exit();
 });
